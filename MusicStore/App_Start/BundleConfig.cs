8363ba89ed83b8d1d73a0e0ece10e945a05449f8
trungkien3289 <trungkien3289@gmail.com>
2018-03-27 23:41:31 +0700

    Update layout for website

--- MusicStore/App_Start/BundleConfig.cs
+++ MusicStore/App_Start/BundleConfig.cs
@@ -25,7 +25,11 @@ namespace MusicStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Common/icons.css",
+                      "~/Content/site.css",
+                      "~/Content/Common/style.css",
+                      "~/Content/Common/font-awesome.css"
+                      ));
         }
     }
 }
