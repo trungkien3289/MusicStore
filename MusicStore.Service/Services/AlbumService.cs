b3092a97c85cb4878c14512318441a5dd9df71b8
trungkien3289 <trungkien3289@gmail.com>
2018-04-05 07:31:15 +0700

    add Repository and UnitOfWork

---
+++ MusicStore.Service/Services/AlbumService.cs
@@ -0,0 +1,44 @@
+﻿using MusicStore.Model.UnitOfWork;
+using MusicStore.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Service.Services
+{
+    public class AlbumService : IAlbumService
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public AlbumService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+
+        public Models.AlbumSummary GetAlbumById(int albumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.AlbumSummary> GetAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CreateAlbum(Models.AlbumSummary albumSummary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateAlbum(int albumId, Models.AlbumSummary albumSummary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAlbum(int albumId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
