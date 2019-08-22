ec026aab29b74b1b84f1fe6d4738fa04cac0abe6
Nin <nintran1995@gmail.com>
2019-08-22 21:18:10 +0700

    Show the projects.

---
+++ WebAPI/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using MusicStore.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class ProjectController : Controller
+    {
+		private IProjectServices _projectServices;
+
+		public ProjectController(IProjectServices projectServices)
+		{
+			this._projectServices = projectServices;
+		}
+		// GET: Project
+		public ActionResult Index()
+		{
+			// get projects
+			var viewModel = _projectServices.GetAll();
+			return View(viewModel);
+		}
+	}
+}
