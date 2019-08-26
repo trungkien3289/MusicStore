ad1fe79678dcbf85a4e7a7e7f86af4f87f87963c
trungkienkaka <trungkienkaka@outlook.com>
2019-08-26 20:02:53 +0700

    Update add task request.

--- MusicStore.BussinessEntity/TaskRequestEntity.cs
+++ MusicStore.BussinessEntity/TaskRequestEntity.cs
@@ -20,7 +20,7 @@ namespace MusicStore.BussinessEntity
         public virtual ProjectEntity Project { get; set; }
         public virtual UserEntity Assignee { get; set; }
         public virtual TaskEntity Task { get; set; }
-        public virtual ICollection<fl_TaskRequestDeveloper> Developers { get; set; }
+        //public virtual ICollection<TaskRequestDeveloperEntity> Developers { get; set; }
     }
 }
 
